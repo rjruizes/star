cb4398f5785a8b562160cda2f45ba138662dd0ce
Roman Ruiz-Esparza <rruizesparza@rti.org>
2021-07-20 14:24:39 -0400

    serve on 0.0.0.0:5000 for docker

--- manage.py
+++ manage.py
@@ -6,15 +6,12 @@ from flask_script.commands import ShowUrls, Clean
 
 from werkzeug.middleware.profiler import ProfilerMiddleware
 
-
-
-
 from star import create_app
 
 
 app = create_app()
 manager = Manager(app)
-manager.add_command('server', Server())
+manager.add_command('server', Server(host="0.0.0.0", port=5000))
 manager.add_command('show-urls', ShowUrls())
 manager.add_command('clean', Clean())
 
